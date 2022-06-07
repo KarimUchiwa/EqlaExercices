7a2dc1553259e6bf0ca761d2b112293a25ce72fb
karim <karim.bouchaib2000@gmail.com>
2022-06-07 11:14:24 +0200

    ajout de l'exercice 2

--- php/c2e6.php
+++ php/c2e6.php
@@ -1,7 +1,35 @@
-<?php
-$phrase = readline("Tapez votre phrase ");
-if (strlen($phrase)==0)
+<?php 
+$phrase = readline("Tapez votre phrase") ;
+$ex = substr($phrase, (strlen($phrase)-1)/2, 1) ;
+$firstw = substr($phrase, 0,1) ;
+$lastw = substr($phrase,-1) ;
+
+function VoyelleConsonne($_par)
 {
-    echo"Débile" ;
+    if ($_par == "a" || $_par == "e" || $_par == "i" || $_par == "o" || $_par == "u" || $_par == "y")
+    {
+        echo " et c'est une voyelle" ;
+    }
+    else 
+    {
+        echo " et c'est une consonne" ;
+    }
+}
+
+if (strlen($phrase)==0) 
+{
+    echo "Debile" ;
+}
+else if (strlen($phrase)%2==0) 
+{
+    echo "le premier caractère est ".$firstw ;
+    VoyelleConsonne($firstw) ;
+    echo " Le dernier caractère est ".$lastw ;
+    VoyelleConsonne($lastw) ;
+}
+else
+{
+    echo "la lettre du millieu est ". $ex ;
+VoyelleConsonne($ex) ;
 }
 ?>
